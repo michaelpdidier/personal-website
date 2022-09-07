d5315445000e7cf0abf29bdbd47a9719f4abdb4f
Michael Didier <18317205+michaelpdidier@users.noreply.github.com>
2022-09-07 15:42:57 -0700

    added landing page and started code editor:

---
+++ src/pages/CodeEditor.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import Button from '../components/Button';
+// import Editor from '../components/Editor';
+
+
+function CodeEditor() {
+
+    const [openedEditor, setOpenedEditor] = useState('html');
+    const [html, setHtml] = useState('');
+    const [css, setCss] = useState('');
+    const [js, setJs] = useState('');
+
+    const onTabClick = (editorName) => {
+        setOpenedEditor(editorName);
+    };
+
+
+    return (
+        <div className="App">
+            <p>Welcome to the editor!</p>
+            <div className="tab-button-container">
+                <Button title="HTML" onClick={() => {
+                    onTabClick('html')
+                }} />
+
+                <Button title="CSS" onClick={() => {
+                    onTabClick('css')
+                }} />
+                <Button title="JavaScript" onClick={() => {
+                    onTabClick('js')
+                }} />
+            </div>
+            <div className="editor-container">
+                {/* {
+                    htmlEditorIsOpen ? (
+                        <Editor
+                            language="xml"
+                            value={html}
+                            setEditorState={setHtml}
+                        />
+                    ) : cssEditorIsOpen ? (
+                        <Editor
+                            language="css"
+                            value={css}
+                            setEditorState={setCss}
+                        />
+                    ) : (
+                        <Editor
+                            language="javascript"
+                            value={js}
+                            setEditorState={setJs}
+                        />
+                    )
+                } */}
+            </div>
+        </div>
+    );
+
+}
+export default CodeEditor;
+
+
---
+++ src/pages/Home.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+import CodeEditor from './CodeEditor';
+
+const Home = () => {
+
+    return (
+        <div className='App'>
+            <h1>Hello.</h1>
+            <h2>Welcome to michaelpdidier.com</h2>
+            <p>This is a landing place for things I'm working on. </p>
+            <hr></hr>
+            <h3>Projects Under Construction</h3>
+            <p><a href="http://didierchocolate.com">Chocolate Website</a></p>
+            <h3>Experimental</h3>
+            <Link to={"./CodeEditor"}>Code Editor</Link>
+            <p><a href="/#">Drag & Drop</a></p>
+            <p><a href="/#">Block Library</a></p>
+        </div>
+
+    );
+
+};
+
+export default Home;
